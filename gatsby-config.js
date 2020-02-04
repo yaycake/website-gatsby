75b22889081f4135dc94fbaf87520e4df8af2ca2
Grace Yang <yaycake@gmail.com>
2020-02-04 21:50:53 +0800

    finished replacing json project model with graphql markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     'gatsby-transformer-json',
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -17,9 +18,15 @@ module.exports = {
       }, 
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'projects', 
+        path: `${__dirname}/src/projects/`
+      }, 
+    },
+    {
       resolve: 'gatsby-source-filesystem', 
       options: {
-        name: `images`,
         path: `${__dirname}/src/images`
       }
     },
